465281aebf72a3212a6557f3e5f30bb5dd25b3e5
osvegn <thomas.prudhomme@epitech.eu>
2022-11-18 21:19:30 +0100

    Create CMakeLists.txt to set src and src_test to run tests Create a unit_test template #17

---
+++ tests/template/template_unit_test.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** gomoku
+** File description:
+** template_unit_test
+*/
+
+#include <criterion/criterion.h>
+
+Test(template_test, template_test)
+{
+    cr_assert_eq(1, 1);
+}
