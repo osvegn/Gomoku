ac1cb75ed964bcc87309a4ecdc46f7aaeb6fe084
osvegn <thomas.prudhomme@epitech.eu>
2022-11-19 00:05:45 +0100

    [CREATE] tests for get start protocol function #1

--- tests/start_protocol_tests.c
+++ tests/start_protocol_tests.c
@@ -15,6 +15,8 @@ static void redirect_all_stdout(void)
     cr_redirect_stderr();
 }
 
+/// answer_start_protocol tests
+
 Test(start_answer_protocol, test_success_value, .init = redirect_all_stdout)
 {
     answer_start_protocol(true, "success");
@@ -33,8 +35,69 @@ Test(answer_start_protocol, test_success_without_message, .init = redirect_all_s
     cr_assert_stdout_eq_str("OK\n");
 }
 
-Test(answer_start_protocol, test_failure_withou_message, .init = redirect_all_stdout)
+Test(answer_start_protocol, test_failure_without_message, .init = redirect_all_stdout)
 {
     answer_start_protocol(false, NULL);
     cr_assert_stdout_eq_str("ERROR\n");
+}
+
+
+/// get_start_protocol tests
+
+Test(get_start_protocol, test_basic_valide_start_protocol)
+{
+    char message[] = "START 7";
+    unsigned int size = 0;
+    int rvalue = get_start_protocol(message, &size);
+
+    cr_assert_eq(rvalue, 0);
+    cr_assert_eq(size, 7);
+}
+
+Test(get_start_protocol, test_negative_start_value)
+{
+    char message[] = "START -2";
+    unsigned int size = 0;
+    int rvalue = get_start_protocol(message, &size);
+
+    cr_assert_eq(rvalue, -1);
+    cr_assert_eq(size, 0);
+}
+
+Test(get_start_protocol, test_invalide_protocol_name)
+{
+    char message[] = "AERFT 20";
+    unsigned int size = 0;
+    int rvalue = get_start_protocol(message, &size);
+
+    cr_assert_eq(rvalue, -1);
+    cr_assert_eq(size, 0);
+}
+
+Test(get_start_protocol, test_get_start_protocol_without_message)
+{
+    char *message = NULL;
+    unsigned int size = 0;
+    int rvalue = get_start_protocol(message, &size);
+
+    cr_assert_eq(rvalue, -1);
+    cr_assert_eq(size, 0);
+}
+
+Test(get_start_protocol, test_get_start_protocol_without_size_pointer)
+{
+    char message[] = "START 23";
+    int rvalue = get_start_protocol(message, NULL);
+
+    cr_assert_eq(rvalue, -1);
+}
+
+Test(get_start_protocol, test_get_start_protocol_with_high_value)
+{
+    char message[] = "START 123456789";
+    unsigned int size = 0;
+    int rvalue = get_start_protocol(message, &size);
+
+    cr_assert_eq(rvalue, 0);
+    cr_assert_eq(size, 123456789);
 }
