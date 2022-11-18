e4077745cfc71c0eeed7515a87e3c3778bf1e19d
osvegn <thomas.prudhomme@epitech.eu>
2022-11-19 00:16:33 +0100

    [CREATE] answer turn protocol function #1

---
+++ sources/turn_protocol.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** gomoku
+** File description:
+** turn_protocol
+*/
+
+#include "gomoku.h"
+
+void answer_turn_protocol(unsigned int x, unsigned int y)
+{
+    my_printf("%i,%i\n", x, y);
+}
