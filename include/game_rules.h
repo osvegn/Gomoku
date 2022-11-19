d644202b1efe21cd133a9a7c3e2262d63b481a88
osvegn <thomas.prudhomme@epitech.eu>
2022-11-19 15:57:18 +0100

    [CREATE] game_rules handling updated by info_protocol #5

---
+++ include/game_rules.h
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** gomoku
+** File description:
+** game_rules
+*/
+
+#ifndef GAME_RULES_H_
+#define GAME_RULES_H_
+
+#include <stddef.h>
+
+typedef struct game_rules_s {
+    const char *key;
+    void *value;
+} game_rules_t;
+
+#endif /* !GAME_RULES_H_ */
---
+++ sources/game_rules.c
@@ -0,0 +1,79 @@
+/*
+** EPITECH PROJECT, 2022
+** gomoku
+** File description:
+** game_rules
+*/
+
+#include "game_rules.h"
+#include <string.h>
+
+static game_rules_t RULES[] =
+{
+    {
+        .key = "timeout_turn",
+        .value = 0
+    },
+    {
+        .key = "timeout_match",
+        .value = 0
+    },
+    {
+        .key = "max_memory",
+        .value = 0
+    },
+    {
+        .key = "time_left",
+        .value = 0
+    },
+    {
+        .key = "game_type",
+        .value = 0
+    },
+    {
+        .key = "rule",
+        .value = 0
+    },
+    {
+        .key = "evaluate",
+        .value = 0
+    },
+    {
+        .key = "folder",
+        .value = 0
+    },
+    {
+        .key = 0,
+        .value = 0
+    }
+};
+
+game_rules_t **get_rules(void)
+{
+    return &RULES;
+}
+
+game_rules_t *get_rule(const char *key)
+{
+    int i = 0;
+
+    while (RULES[i].key) {
+        if (!strcmp(RULES[i].key, key))
+            return &RULES[i];
+        i++;
+    }
+    return NULL;
+}
+
+int set_rule(const char *key, const void *value)
+{
+    int i = 0;
+
+    while (RULES[i].key) {
+        if (!strcmp(RULES[i].key, key)) {
+            RULES[i].value = value;
+            return 0;
+        }
+    }
+    return -1;
+}
