84a2b92e739a7c47c1bbb03e0acbb7c6929ae14b
osvegn <thomas.prudhomme@epitech.eu>
2022-11-19 14:52:54 +0100

    [CHANGE] readstdin function to readfile function #30

---
+++ sources/readfile.c
@@ -0,0 +1,41 @@
+/*
+** EPITECH PROJECT, 2022
+** gomoku
+** File description:
+** readstdin
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int allocate_memory(char **buffer, size_t *size)
+{
+    (*size) += 128;
+    (*buffer) = realloc((*buffer), *size);
+    if (!(*buffer))
+        return -1;
+    return 0;
+}
+
+int readfile(char **lineptr, size_t *n, FILE *stream)
+{
+    char *buffer = NULL;
+    size_t size = 0;
+    int c = 0;
+    int i = 0;
+
+    if (!lineptr || !stream || !n)
+        return -1;
+    c = fgetc(stream);
+    while (c != EOF) {
+        if (size == 0 || size - 1 <= i) {
+            allocate_memory(&buffer, &size);
+        }
+        buffer[i] = c;
+        i++;
+        buffer[i] = '\0';
+        c = fgetc(stream);
+    }
+    *n = size;
+    *lineptr = buffer;
+    return i;
+}
