6b683393bfe093023208ae3ae896ba5992a04faf
osvegn <thomas.prudhomme@epitech.eu>
2022-11-21 22:22:30 +0100

    [TEST] find victory condition #39

--- sources/find_victory.c
+++ sources/find_victory.c
@@ -7,23 +7,56 @@
 
 #include "board.h"
 
-void find_piece(unsigned int *i, unsigned int *j, const board_t *board)
+int get_offset(int direction, unsigned int size)
 {
-    for (; (*i) < board->size && board->board[(*i)][(*j)]; (*i)++) {
-        for (; (*j) < board->size && board->board[(*i)][(*j)]; (*j)++);
+    switch (direction) {
+        case 1:
+            return -(size - 1);
+        case 2:
+            return 1;
+        case 3:
+            return size + 1;
+        case 4:
+            return size;
+        default:
+            return -1;
     }
+    return -1;
+}
+
+int find_directions(const board_t *board, unsigned int i, int direction, int player)
+{
+    int offset = get_offset(direction, board->size);
+    unsigned int len = 1;
+    unsigned int tmp = i;
+
+    while (i + offset > 0 && i + offset < board->size * board->size && board->board[i + offset] == player) {
+        i += offset;
+        len++;
+        if (len == 4 &&
+        (i + offset > 0 && i + offset < board->size * board->size && board->board[i + offset] == 0) ||
+        (i - (offset * 4) > 0 && i - (offset * 4) < board->size * board->size && board->board[i - (offset * 4)] == 0))
+            return direction;
+    }
+    if (direction < 4)
+        return find_directions(board, tmp, direction + 1, player);
+    return 0;
 }
 
 int is_victory_available(void)
 {
     const board_t *board = get_board();
     unsigned int i = 0;
-    unsigned int j = 0;
+    int rvalue = 0;
 
     while (i < board->size) {
-        while (j < board->size) {
-            find_piece(&i, &j, board);
+        if (board->board[i]) {
+            rvalue = find_directions(board, i, 1, board->board[i]);
+            my_printf("Find piece\n");
         }
+        if (rvalue)
+            return rvalue;
+        i++;
     }
     return 0;
 }
