57078f1373e89802c62e1724ca146df4b3cdc6cd
osvegn <thomas.prudhomme@epitech.eu>
2022-11-21 17:40:04 +0100

    [USE] int * instead of int ** for the board size #40

--- sources/board.c
+++ sources/board.c
@@ -12,35 +12,18 @@ static board_t board = {NULL, 0};
 
 void destroy_board(void)
 {
-    unsigned int i = 0;
-
-    if (!board.board) {
-        return;
-    }
-    while (i < board.size) {
-        if (board.board[i]) {
-            free(board.board[i]);
-        }
-        i++;
-    }
-    free(board.board);
+    if (board.board)
+        free(board.board);
 }
 
 int create_board(unsigned int size)
 {
-    unsigned int i = 0;
-
     destroy_board();
-    board.board = malloc(sizeof(int *) * (size));
+    board.board = malloc(sizeof(int) * (size * size));
     if (!board.board)
         return -1;
-    while (i < size) {
-        board.board[i] = malloc(sizeof(int) * (size));
-        if (!board.board[i])
-            return -1;
-        for (unsigned int j = 0; j < size; j++)
-            board.board[i][j] = 0;
-        i++;
+    for (unsigned int i = 0; i < size * size; i++) {
+        board.board[i] = 0;
     }
     board.size = size;
     return 0;
@@ -50,7 +33,7 @@ int add_piece_to_board(unsigned int x, unsigned int y, unsigned int player)
 {
     if (!board.board || x >= board.size || y >= board.size || player == 0)
         return -1;
-    board.board[y][x] = player;
+    board.board[y * board.size + x] = player;
     return 0;
 }
 
@@ -58,7 +41,7 @@ int remove_piece_to_board(unsigned int x, unsigned int y)
 {
     if (!board.board || x >= board.size || y >= board.size)
         return -1;
-    board.board[y][x] = 0;
+    board.board[y * board.size + x] = 0;
     return 0;
 }
 
