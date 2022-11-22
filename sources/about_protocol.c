f94a43f337a3a3eaf033871df7e08cefce0644a7
osvegn <thomas.prudhomme@epitech.eu>
2022-11-22 15:40:54 +0100

    [ADD] about protocol #33

---
+++ sources/about_protocol.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2022
+** gomoku
+** File description:
+** about_protocol
+*/
+
+int answer_about_protocol(const char *message)
+{
+    const char *name = "NOUS";
+    const char *author = "NOUS";
+
+    my_printf("name=\"%s\", version=\"1.0\", author=\"%s\", country=\"USA\"", name, author);
+    return 0;
+}
