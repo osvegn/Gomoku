fa160b1d52aeadd12e2d251301c8c96695c08c58
thomas <thomas.prudhomme@epitech.eu>
2022-11-30 23:53:11 +0100

    [TRY] to find pattern

--- sources/IA.c
+++ sources/IA.c
@@ -55,7 +55,7 @@ void get_dumb_ia(scoords_t *s_coordinates)
     s_coordinates->y = i / board->size;
 }
 
-void get_ia(scoords_t* s_coordinates)
+void get_ia2(scoords_t* s_coordinates)
 {
     const board_t* board = get_board();
     int rvalue = 0;
@@ -76,4 +76,44 @@ void get_ia(scoords_t* s_coordinates)
     }
     if (s_coordinates->x <= 0 || s_coordinates->y <= 0)
         get_dumb_ia(s_coordinates);
+}
+
+#include "vector.h"
+typedef struct pattern_info_s {
+    unsigned int id;
+    unsigned int direction;
+    unsigned int position;
+} pattern_info_t;
+
+static int print_int(void *data)
+{
+    pattern_info_t *value = (pattern_info_t *)data;
+
+    my_printf("[id: %i, direction: %i, position: %i]", value->id, value->direction, value->position);
+    return 0;
+}
+
+int find_pattern(pattern_info_t *info, int *i, const board_t *board)
+{
+    if (board->board[*i]) {
+        (*info) = (pattern_info_t){0, 0, *i};
+        return 1;
+    }
+    return 0;
+}
+
+void get_ia(scoords_t* s_coordinates)
+{
+    const board_t *board = get_board();
+    vector_t vector;
+    pattern_info_t info;
+
+    vector_constructor(&vector, sizeof(int), 100);
+    for (unsigned int i = 0; i < board->size * board->size; i++) {
+        if (find_pattern(&info, &i, board))
+            vector.emplace_back(&vector, &info);
+    }
+    vector.print(&vector, &print_int);
+    vector.destructor(&vector);
+    get_ia2(s_coordinates);
 }
