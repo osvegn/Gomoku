f6229face8c6504eeea30ab4412c9d817b0c2046
thomas <thomas.prudhomme@epitech.eu>
2022-11-30 17:40:51 +0100

    [ADD] more tests for vector

--- tests/vector_tests.c
+++ tests/vector_tests.c
@@ -8,26 +8,55 @@
 #include <criterion/criterion.h>
 #include "vector.h"
 
-Test(vector, global_test)
+Test(vector_creation, constructor)
+{
+    vector_t vector;
+
+    vector_constructor(&vector, sizeof(int), 0);
+    vector.destructor(&vector);
+    cr_assert_eq(1, 1);
+}
+
+Test(vector_emplace, emplace)
+{
+    vector_t vector;
+    int data = 10;
+
+    vector_constructor(&vector, sizeof(int), 0);
+    vector.emplace(&vector, &data, 0);
+    vector.destructor(&vector);
+    cr_assert_eq(1, 1);
+}
+
+Test(vector_emplace_back, emplace_back)
 {
     vector_t vector;
     int data = 10;
 
     vector_constructor(&vector, sizeof(int), 0);
     vector.emplace_back(&vector, &data);
-    data += 5;
-    vector.emplace(&vector, &data, 0);
-    cr_assert_eq((int) *(int *)vector.at(&vector, 1), 10);
-    cr_assert_eq((int) *(int *)vector.front(&vector), 5);
-    cr_assert_eq((int) *(int *)vector.back(&vector), 10);
-    // cr_assert_eq(vector.empty(&vector), false);
-    cr_assert_eq(vector.get_size(&vector), 2);
-    cr_assert_eq(vector.get_capacity(&vector), 2);
-    vector.clear(&vector);
-    // cr_assert_eq(vector.empty(&vector), true);
-    cr_assert_eq(vector.get_size(&vector), 0);
-    cr_assert_eq(vector.get_capacity(&vector), 2);
+    vector.destructor(&vector);
+    cr_assert_eq(1, 1);
+}
 
+Test(vector_size, size)
+{
+    vector_t vector;
+    int data = 10;
+
+    vector_constructor(&vector, sizeof(int), 0);
+    vector.emplace_back(&vector, &data);
+    cr_assert_eq(vector.get_size(&vector), 1);
+    vector.destructor(&vector);
+    cr_assert_eq(1, 1);
+}
+
+Test(vector_capacity, capacity)
+{
+    vector_t vector;
+
+    vector_constructor(&vector, sizeof(int), 10);
+    cr_assert_eq(vector.get_capacity(&vector), 10);
     vector.destructor(&vector);
     cr_assert_eq(1, 1);
 }
