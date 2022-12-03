6cade3f815a6667aa5951796d56e9ac97c132a93
Nekomay <maylis.ducournau@epitech.eu>
2022-12-03 23:25:57 +0100

    [MODIFY] about print

--- sources/about_protocol.c
+++ sources/about_protocol.c
@@ -9,9 +9,9 @@
 
 int answer_about_protocol(const char *UNUSED(message))
 {
-    const char *name = "NOUS";
-    const char *author = "NOUS";
+    const char *name = "pbrain-gomoku-ai";
+    const char *author = "osvegn and Nekomay";
 
-    my_printf("name=\"%s\", version=\"1.0\", author=\"%s\", country=\"USA\"\n", name, author);
+    my_printf("name=\"%s\", version=\"1.0\", author=\"%s\", country=\"France\"\n", name, author);
     return 0;
 }
